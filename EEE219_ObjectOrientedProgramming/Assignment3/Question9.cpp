f5cf748805282ac081f6170f1a0fd07a5bb78062
Izzy Fox <93491659+isobelfoxx@users.noreply.github.com>
2022-10-06 22:38:07 +0100

    Create Question9.cpp

---
+++ EEE219_ObjectOrientedProgramming/Assignment3/Question9.cpp
@@ -0,0 +1,23 @@
+// Question 9
+#include <iostream>
+using namespace std;
+
+class Test
+{
+private:
+  int a, b;
+
+public:
+    void setvalues (int a, int b)
+  {
+this -> a = a;
+this -> b = b;
+  }
+};
+
+
+int
+main ()
+{
+  return 0;
+}
