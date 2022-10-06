6b6d05fb0e4c2373cd1bb85ab9d3b111db693cf0
Izzy Fox <93491659+isobelfoxx@users.noreply.github.com>
2022-10-06 22:17:31 +0100

    Create Question4.cpp

---
+++ EEE219/Assignment1/Question4.cpp
@@ -0,0 +1,16 @@
+Question 4
+
+#include <iostream>
+
+using namespace std;
+
+int
+main ()
+{
+    double decimalnumber = 10.34548;
+    int intnumber;
+    // minusing the integer value of decimalnumber by the
+    // double value giving the fractional value of the double
+    double frac = decimalnumber - (int)decimalnumber;
+    cout << frac;
+}
